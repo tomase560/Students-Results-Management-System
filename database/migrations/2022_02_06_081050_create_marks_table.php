77637cef17bd42f5b82e9cd348d84e8469d1942b
tomase560 <95083395+tomase560@users.noreply.github.com>
2022-02-14 12:09:35 +0200

    Add files via upload

---
+++ database/migrations/2022_02_06_081050_create_marks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->string('studID');
+            $table->string('courseID');
+            $table->double('marks', 3, 1);
+            $table->timestamps();
+
+            $table->foreign('courseID')->references('courseID')->on('courses')->onDelete('cascade');
+            $table->foreign('StudID')->references('StudID')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+}
